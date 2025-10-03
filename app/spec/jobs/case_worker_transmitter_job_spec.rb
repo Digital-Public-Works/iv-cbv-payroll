0371c1cc867a0f9b2e5790fbc51274ce6b346663
jeffcatania-dpw <jeff@digitalpublicworks.org>
2025-10-03 13:41:14 -0400

    fixed a semi-related test that was breaking because it was trying to grab sftp information for sandbox agency which does not have configuration.rb

--- app/spec/jobs/case_worker_transmitter_job_spec.rb
+++ app/spec/jobs/case_worker_transmitter_job_spec.rb
@@ -20,7 +20,8 @@ RSpec.describe CaseWorkerTransmitterJob, type: :job do
       :with_pinwheel_account,
       with_errored_jobs: errored_jobs,
       created_at: current_time - 10.minutes,
-      cbv_applicant: cbv_applicant
+      cbv_applicant: cbv_applicant,
+      client_agency_id: "pa_dhs" # TODO: this needs to be not agency-specific
     )
   end
 
