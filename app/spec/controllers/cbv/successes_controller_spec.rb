abfb5293befbeab111525d91398fb2e9070b48cf
Tim Miller <timmiller@navapbc.com>
2025-10-08 16:24:34 -0500

    cp 4aa6890bdca36de3864e9f05b8713ce0b465af02

--- app/spec/controllers/cbv/successes_controller_spec.rb
+++ app/spec/controllers/cbv/successes_controller_spec.rb
@@ -48,7 +48,7 @@ RSpec.describe Cbv::SuccessesController do
           it "uses agency demo domain with shared origin" do
             get :show
 
-            expected_url = "https://sandbox.#{ENV["DOMAIN_NAME"]}/en/cbv/entry?origin=shared&amp;token=#{cbv_flow.cbv_flow_invitation.auth_token}"
+            expected_url = "https://sandbox.#{ENV["DOMAIN_NAME"]}/en/start/#{cbv_flow.cbv_flow_invitation.auth_token}?origin=shared"
             expect(response.body).to include(expected_url)
           end
         end
@@ -60,9 +60,7 @@ RSpec.describe Cbv::SuccessesController do
 
           it "uses agency demo domain with shared origin" do
             get :show
-            expected_url = "https://demo.divt.app.#{ENV["DOMAIN_NAME"]}/en/cbv/entry?origin=shared&amp;token=#{cbv_flow.cbv_flow_invitation.auth_token}"
-
-            puts "EXPECTED URL: #{expected_url}"
+            expected_url = "https://demo.divt.app.#{ENV["DOMAIN_NAME"]}/en/start/#{cbv_flow.cbv_flow_invitation.auth_token}?origin=shared"
             expect(response.body).to include(expected_url)
           end
         end
