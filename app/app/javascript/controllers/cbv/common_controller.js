92ac14975b811ff4f5f44c3211f4c195c32ebea6
Runako Godfrey <runako@climb.local>
2025-11-25 17:14:38 -0500

    Fixing indents.

--- app/app/javascript/controllers/cbv/common_controller.js
+++ app/app/javascript/controllers/cbv/common_controller.js
@@ -1,16 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  initialize() {
-    console.log("Common controller initialized.")
-  }
-  
   connect() {
-    console.log("Common controller connected.")
   }
 
   disconnect() {
-    console.log("Commong controller disconnected.")
   }
 
   back() {
--- app/db/schema.rb
+++ app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_15_183520) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_12_170544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -38,7 +38,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_15_183520) do
     t.string "client_agency_id"
     t.jsonb "income_changes"
     t.date "date_of_birth"
-    t.uuid "case_guid"
     t.string "doc_id"
   end
 
