be73d3de3106b3b3ff570cdad8fcdaeac87311d2
Jeff Catania <jeff@digitalpublicworks.org>
2025-12-16 19:20:37 -0500

    fixes scrolling issues with missing_results controller anchor tags

--- app/app/javascript/controllers/index.js
+++ app/app/javascript/controllers/index.js
@@ -1,5 +1,7 @@
 import { application } from "./application"
 
+import AnchorScrollController from "./anchor_scroll_controller.js"
+import ScrollResetController from "./scroll_reset_controller.js"
 import BackButtonController from "./cbv/back_button_controller.js"
 import CbvEmployerSearch from "./cbv/employer_search"
 import SessionTimeoutPageController from "./cbv/sessions_timeout_controller.js"
@@ -12,6 +14,8 @@ import CbvEntryPageController from "./cbv/entry_page_controller.js"
 import PreviewFormController from "./preview_form_controller.js"
 import ClickTrackerController from "./click_tracker_controller.js"
 
+application.register("anchor-scroll", AnchorScrollController)
+application.register("scroll-reset", ScrollResetController)
 application.register("back-button", BackButtonController)
 application.register("cbv-employer-search", CbvEmployerSearch)
 application.register("click-tracker", ClickTrackerController)
