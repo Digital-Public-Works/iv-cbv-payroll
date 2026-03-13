82482db8638abc3bbd366cfa0c622b708abde8da
DianaLiao <dijali@gmail.com>
2026-03-13 10:28:23 -0400

    address code review comments

--- app/app/components/report/client_comment_component.rb
+++ app/app/components/report/client_comment_component.rb
@@ -9,8 +9,11 @@ class Report::ClientCommentComponent < ViewComponent::Base
   end
 
   private
+  CHAT_ICON_PATH = "M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zM6 9h12v2H6V9zm8 5H6v-2h8v2zm4-6H6V6h12v2z".freeze
 
   def chat_icon_path
-    "M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zM6 9h12v2H6V9zm8 5H6v-2h8v2zm4-6H6V6h12v2z"
+    # Note: Using inline SVG path instead of sprite reference
+    # because wicked_pdf cannot resolve external sprite.svg files
+    CHAT_ICON_PATH
   end
 end
