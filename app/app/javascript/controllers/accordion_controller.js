f8ed492c7ce9397d6c2edb8ece2866b875aeaa4b
DianaLiao <dijali@gmail.com>
2026-04-01 14:28:10 -0400

    get rid of uswds accordion override

--- app/app/javascript/controllers/accordion_controller.js
+++ app/app/javascript/controllers/accordion_controller.js
@@ -3,25 +3,23 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["trigger", "content"]
 
-  setContentExpansion(event) {
-    event.stopImmediatePropagation()
-    const isExpanded = this.triggerTarget.getAttribute("aria-expanded") === "true"
-
-    if (isExpanded) {
-      this.collapseContent()
-    } else {
-      this.expandContent()
-    }
+  static values = {
+    expanded: { type: Boolean, default: false },
+    clickedToOpen: { type: Boolean, default: false },
   }
-  expandContent() {
-    this.contentTarget.hidden = false
-    this.triggerTarget.setAttribute("aria-expanded", "true")
-    this.triggerTarget.setAttribute("data-context-clicked-to-open", "true")
+
+  expandedValueChanged(value) {
+    this.clickedToOpenValue = !this.clickedToOpenValue
+    this.triggerTarget.setAttribute("data-context-clicked-to-open", this.clickedToOpenValue)
+  }
+
+  setContentExpansion(event) {
+    this.expandedValue = !this.expandedValue
   }
 
   collapseContent() {
     this.contentTarget.hidden = true
     this.triggerTarget.setAttribute("aria-expanded", "false")
-    this.triggerTarget.setAttribute("data-context-clicked-to-open", "false")
+    this.expandedValue = false
   }
 }
