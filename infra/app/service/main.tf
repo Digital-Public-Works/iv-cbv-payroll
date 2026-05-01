0afe59eb80c9ae8acd53240f6892d4324d48ea89
Andre Diggins <andrediggins@gmail.com>
2026-05-01 14:25:22 -0700

    update s3 static so that it is in prod only, demo tf will work, csp will work with demo pointing to prod, and domain prefix has a guard

--- infra/app/service/main.tf
+++ infra/app/service/main.tf
@@ -119,7 +119,8 @@ data "aws_iam_policy" "migrator_db_access_policy" {
 }
 
 data "aws_iam_policy" "static_assets_access" {
-  name = "dpw-${module.project_config.project_name}-static-assets-access"
+  count = var.environment_name == "prod" ? 1 : 0
+  name  = "dpw-${module.project_config.project_name}-static-assets-access"
 }
 
 resource "aws_iam_policy" "email_access_policy" {
@@ -223,10 +224,12 @@ module "service" {
 
   extra_environment_variables = merge(
     {
-      BUCKET_NAME               = local.storage_config.bucket_name
-      STATIC_ASSETS_BUCKET_NAME = "dpw-${module.project_config.project_name}-static-assets"
-      STATIC_ASSETS_CDN_URL     = "https://static.verifymyincome.org"
+      BUCKET_NAME           = local.storage_config.bucket_name
+      STATIC_ASSETS_CDN_URL = "https://static.verifymyincome.org"
     },
+    var.environment_name == "prod" ? {
+      STATIC_ASSETS_BUCKET_NAME = "dpw-${module.project_config.project_name}-static-assets"
+    } : {},
     local.ssm_env_vars,
     local.identity_provider_environment_variables,
     local.service_config.extra_environment_variables
@@ -245,11 +248,13 @@ module "service" {
 
   extra_policies = merge(
     {
-      storage_access       = module.storage.access_policy_arn,
-      static_assets_access = data.aws_iam_policy.static_assets_access.arn,
-      email_access         = aws_iam_policy.email_access_policy.arn,
-      sqs_queues_access    = module.service.sqs_access_policy_arn
+      storage_access    = module.storage.access_policy_arn,
+      email_access      = aws_iam_policy.email_access_policy.arn,
+      sqs_queues_access = module.service.sqs_access_policy_arn
     },
+    var.environment_name == "prod" ? {
+      static_assets_access = data.aws_iam_policy.static_assets_access[0].arn,
+    } : {},
     module.app_config.enable_identity_provider ? {
       identity_provider_access = module.identity_provider_client[0].access_policy_arn,
     } : {}
