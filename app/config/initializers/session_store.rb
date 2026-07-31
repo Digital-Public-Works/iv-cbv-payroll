c1cdce9cfa1624e67a54f913298f83d641e4df7e
Jeff Catania <jeff@digitalpublicworks.org>
2026-07-31 18:17:28 -0400

    Revert "skip session expiry in e2e tests"
    This reverts commit 17e69466bf35dea0232afc54c0d0c610f1c577fa.

--- app/config/initializers/session_store.rb
+++ app/config/initializers/session_store.rb
@@ -1,7 +1,3 @@
-# Skip session expiry in E2E runs
-# TODO: if we ever want e2e coverage of expiry behavior, we should revisit
-if ENV["E2E_RUN_TESTS"].nil?
-  Rails.application.config.session_store :cookie_store,
-    key: "_iv_cbv_payroll_session",
-    expire_after: Rails.application.config.cbv_session_expires_after
-end
+Rails.application.config.session_store :cookie_store,
+  key: "_iv_cbv_payroll_session",
+  expire_after: Rails.application.config.cbv_session_expires_after
