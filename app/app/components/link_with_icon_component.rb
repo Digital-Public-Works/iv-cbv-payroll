9229699745c4a5ad1b5beca7c3c00b3ce99aa09c
Drew Nickerson <dnick13@gmail.com>
2026-09-08 15:20:45 -0400

    Removing override css and fixing alignment with USWDS classes

--- app/app/components/link_with_icon_component.rb
+++ app/app/components/link_with_icon_component.rb
@@ -47,7 +47,7 @@ class LinkWithIconComponent < ViewComponent::Base
     return unless icon
     icon_sprite_path = helpers.asset_path("@uswds/uswds/dist/img/sprite.svg")
     icon_path = "#{icon_sprite_path}##{icon}"
-    content_tag(:svg, class: "usa-icon", "aria-hidden": true, focusable: false, role: "img") do
+    content_tag(:svg, class: "usa-icon text-middle#{link_classes.exclude?('usa-button') ? ' margin-left-05 margin-bottom-05' : ''}", "aria-hidden": true, focusable: false, role: "img") do
       tag.use("", href: icon_path)
     end
   end
